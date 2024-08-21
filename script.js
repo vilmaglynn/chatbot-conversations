537bd1f14670cd441a1e2ba94b2b3c00982bf9c1
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-08-21 08:49:48 +0100

    improved UI

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const images = {
     female: document.getElementById("female"),
     male: document.getElementById("male"),
-    other: document.getElementById("other")
+    other: document.getElementById("other"),
+    scary: document.getElementById("scary"),
+    cartoon: document.getElementById("cartoon")
   };
 
   // Listen for any change on the radio buttons
@@ -24,3 +26,60 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document
+  .getElementById("voiceInputButton")
+  .addEventListener("click", function () {
+    var recognition = new (window.SpeechRecognition ||
+      window.webkitSpeechRecognition)();
+    recognition.lang = "en-US";
+    recognition.start();
+
+    recognition.onresult = function (event) {
+      document.getElementById("userInput").value =
+        event.results[0][0].transcript;
+    };
+
+    recognition.onerror = function (event) {
+      alert("Error occurred in recognition: " + event.error);
+    };
+  });
+
+// Array of image paths
+const images = [
+  "/images/backgrounds/background-blue3.jpg",
+  "/images/backgrounds/background-blue1.jpg",
+  "/images/backgrounds/background-blue2.jpg",
+  "/images/backgrounds/background-red.jpg",
+  "/images/backgrounds/background-white.jpg"
+];
+
+let currentIndex = 0; // Start at the first image
+
+let background = document.getElementById("background");
+background.addEventListener("click", changeBackground);
+
+function changeBackground(event) {
+  event.preventDefault(); // Keep this if the button is inside a form or an anchor tag
+
+  console.log("clicked");
+
+  // Get the current image
+  const selectedImage = images[currentIndex];
+
+  // Apply the current image as the background
+  document.body.style.backgroundImage = `url('${selectedImage}')`;
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundSize = "cover";
+
+  // Move to the next image in the array
+  currentIndex++;
+
+  // If we've reached the end of the array, start over from the beginning
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+}
+
+// AIzaSyAjpuQ2GoKX1c3zPou9PglZp9Yn88KgxpU
